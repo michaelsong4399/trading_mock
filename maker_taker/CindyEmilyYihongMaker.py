bd702a52cdcaaa8cfdf68f65d86ed857a3987680
Yihong Song <ys377@duke.edu>
2023-02-07 03:08:32 -0500

    Update Maker

--- maker_taker/CindyEmilyYihongMaker.py
+++ maker_taker/CindyEmilyYihongMaker.py
@@ -1,26 +1,8 @@
 from maker_taker_base import Taker, Maker, Side, Market, test
-import random
 from typing import Type
 
 
-class RandomTaker(Taker):
-    def do_trade(self, market: Type[Market], round: int) -> Type[Side]:
-        return random.choice([Side.buy, Side.sell])
-
-
-class GreedyTaker(Taker):
-    def do_trade(self, market: Type[Market], round: int) -> Type[Side]:
-        return market.better_side(self.true_value)
-
-
-class CenterMaker(Maker):
-    def get_market(self, round: int) -> Market():
-        return Market(0, 1, 10, 1)
-
-    def update_trade(self, side: Type[Side]) -> None:
-        return
-
-class BetterMaker(Maker):
+class CindyEmilyYihongMaker(Maker):
     side_map = {Side.buy: 1, Side.sell: -1, Side.notrade: 0}
 
     def __init__(self) -> None:
@@ -55,27 +37,3 @@ class BetterMaker(Maker):
                     self.bounds = (30,1,40,1)
 
         return
-                    
-
-
-
-class FaderMaker(Maker):
-    side_map = {Side.buy: 1, Side.sell: -1, Side.notrade: 0}
-
-    def __init__(self) -> None:
-        self.fair = 50
-
-    def get_market(self, round: int) -> Market():
-        return Market(self.fair - 5, 1, self.fair + 5, 1)
-
-    def update_trade(self, side: Type[Side]) -> None:
-        self.fair += self.side_map[side] * 10
-
-
-def main():
-    test(GreedyTaker, CenterMaker)
-    # test(GreedyTaker, FaderMaker)
-
-
-if __name__ == "__main__":
-    main()
